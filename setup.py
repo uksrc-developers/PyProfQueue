c7fbf94c473995758b717c5ae9d7c54a146ee2ef
Marcus-Keil <marcusk050291@gmail.com>
2024-04-09 14:36:32 +0100

    Amended setup.py for PyPi upload

--- setup.py
+++ setup.py
@@ -3,29 +3,24 @@ from setuptools import setup
 # read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "ReadMe.md").read_text()
 
 setup(
     name='PyProfQueue',
-    version='0.1',
-    description='A python package to inject profiling initialisation into bash scripts, translate queue options and '
-                'submit jobs',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    version='0.1c0',
     url='https://github.com/Marcus-Keil/PyProfQueue',
     author='Marcus Keil',
     author_email='marcusk050291@gmail.com',
     license='MIT License',
     packages=['PyProfQueue'],
     package_dir={'PyProfQueue': 'PyProfQueue'},
-    package_data={'PyProfQueue': ['data/*.txt', 'data/read_prometheus.py']},
+    package_data={'PyProfQueue': ['../ReadMe.md', 'data/*.txt', 'data/read_prometheus.py']},
     install_requires=[
         'numpy',
         'pytz',
         'promql_http_api==0.3.3',
         'pandas<=2.2.1',
     ],
-
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
@@ -33,4 +28,8 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.10',
     ],
+    description='A python package to inject profiling initialisation into bash scripts, translate queue options and '
+                'submit jobs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
