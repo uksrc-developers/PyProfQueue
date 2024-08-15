7d252d9477d3706ff503a23cbe4596d042c5f7b6
Marcus-Keil <marcusk050291@gmail.com>
2024-08-15 15:07:08 +0100

    Fixed description in setup and init

--- PyProfQueue/__init__.py
+++ PyProfQueue/__init__.py
@@ -4,8 +4,8 @@ from .script import *
 """
 PyProfQueue.
 
-PyProfQueue serves as a python package that can take in existing bash scripts, add prometheus monitoring calls and 
-likwid performance measure calls, and submit the script to an HPC queue system on the users' behalf.
+PyProfQueue serves as a python package that can take in existing bash scripts, add initialisations and calls to use 
+profilers on the bash script and submit the script to an HPC queue system on the users' behalf.
 """
 
 __version__ = "0.3.0"
--- setup.py
+++ setup.py
@@ -34,9 +34,9 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.10',
     ],
-    description="PyProfQueue serves as a python package that can take in existing bash scripts, add prometheus "
-                "monitoring calls and likwid performance measure calls, and submit the script to an HPC queue "
-                "system on the users' behalf.",
+    description="PyProfQueue serves as a python package that can take in existing bash scripts, add initialisations "
+                "and calls to use profilers on the bash script, and submit the script to an HPC queue system on the "
+                "users' behalf.",
     long_description=long_description,
     long_description_content_type='text/markdown',
 )
