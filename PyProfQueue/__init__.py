c0ecd4c0f1ac2032e0f51b168c5c3e3406e63c8e
Marcus-Keil <marcusk050291@gmail.com>
2024-11-08 15:42:49 +0000

    IO Plotting added

--- PyProfQueue/__init__.py
+++ PyProfQueue/__init__.py
@@ -8,6 +8,6 @@ PyProfQueue serves as a python package that can take in existing bash scripts, a
 profilers on the bash script and submit the script to an HPC queue system on the users' behalf.
 """
 
-__version__ = "0.3.5a0"
+__version__ = "0.3.5a1"
 __author__ = 'Marcus Keil'
 __credits__ = 'University College London, UK SKA Regional Centre'
