fa6a0a88dc9b981aa3efe93d5474d833ac0b1681
Marcus-Keil <marcusk050291@gmail.com>
2024-11-08 10:05:42 +0000

    IO Plotting added

--- PyProfQueue/submission.py
+++ PyProfQueue/submission.py
@@ -11,7 +11,45 @@ def submit(script: Script,
            tmp_profile_script: str = './tmp_profilefile.sh',
            bash_options: list = None,
            leave_scripts: bool = False,
-           submit: bool = True,
+           test: bool = False):
+    '''
+    Create the temporary profile file and temporary work script in order to submit them to the appropriate queuing
+    system.
+
+    Parameters
+    ----------
+    script : pyprofbash.Script
+        pyprofbash.Script created prior to submission.
+    tmp_work_script : str = './tmp_workfile.sh'
+        Optional Parameter to change the name of the temporary work script which should be the original script
+        used to initiate the Script object, but all queue options should have been removed.
+    tmp_profile_script : str = './tmp_profilefile.sh'
+        Optional Parameter to change the name of the temporary profile script which will be submitted to the queue
+    bash_options : list = ['']
+        Optional parameter to add additional strings to the end of the call of the original work script in case
+        that script has options it needs to have passed to it.
+    leave_scripts : bool = True
+        If True, leave scripts after submission.
+    test : bool =  False
+        If True, leaves scripts that are created, but does not submit them. Instead, it prints out the command it would
+        have used if it had submitted them.
+    '''
+    write_files(script, tmp_work_script, tmp_profile_script, bash_options, leave_scripts)
+
+    if test:
+        print('The following command would be used to submit a job to the queue:')
+        print(' '.join([getattr(script, 'submission'), getattr(script, 'tmp_profile_script')]))
+    else:
+        subprocess.run(' '.join([getattr(script, 'submission'), getattr(script, 'tmp_profile_script')]), shell=True)
+        time.sleep(1)
+    return
+
+
+def write_files(script: Script,
+           tmp_work_script: str = './tmp_workfile.sh',
+           tmp_profile_script: str = './tmp_profilefile.sh',
+           bash_options: list = None,
+           leave_scripts: bool = False,
            test: bool = False):
     '''
     Create the temporary profile file and temporary work script in order to submit them to the appropriate queuing
@@ -38,12 +76,6 @@ def submit(script: Script,
     if bash_options is None:
         bash_options = ['']
     script.create_profilefile(bash_options)
-    if test:
-        print('The following command would be used to submit a job to the queue:')
-        print(' '.join([getattr(script, 'submission'), getattr(script, 'tmp_profile_script')]))
-    else:
-        subprocess.run(' '.join([getattr(script, 'submission'), getattr(script, 'tmp_profile_script')]), shell=True)
-        time.sleep(1)
 
     if leave_scripts or test:
         time.sleep(1)
