2cc5c9fa3fb47f19d4ee830f60198a2f3695a9d6
Marcus-Keil <marcusk050291@gmail.com>
2024-12-04 14:19:38 +0000

    Updated some naming to adhere with new PyPi standard

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "ReadMe.md").read_text()
 
 setup(
-    name='PyProfQueue',
-    version='0.3.6a7',
+    name='pyprofqueue',
+    version='0.3.6a8',
     url='https://github.com/uksrc-developers/PyProfQueue',
     author='Marcus Keil',
     author_email='marcusk050291@gmail.com',
