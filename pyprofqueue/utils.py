baa7552ebe40ffd7fb1e609191d1cfd7ee6bb42e
Marcus-Keil <marcusk050291@gmail.com>
2025-07-16 11:00:37 +0100

    utils update and correction for slurm profiling.

--- pyprofqueue/utils.py
+++ pyprofqueue/utils.py
@@ -114,51 +114,72 @@ def get_job_details(job_path: str, slurm_path :str) -> dict:
     return job_info
 
 
-def get_profiling_data(job_path: str):
+def get_profiling_data_by_path(job_path: str):
     slurm_path = job_path + "/slurm_profiling"
     job_info = get_job_details(job_path, slurm_path)
     job_info = get_multiple_dataframes(slurm_path, job_info)
     return job_info
 
+def get_profiling_data_by_id(job_id, sbatch_script):
+    if not os.path.isfile(f"{job_id}_profile.h5"):
+        subprocess.run(f'sh5util -S -j {job_id} -o {job_id}_profile.h5', shell=True)
+    job_info = {"job_id": job_id}
+    with open(f'{sbatch_script}', 'r') as file:
+        for line in file:
+            if "#SBATCH " in line:
+                if "--mem" in line:
+                    job_info["memory"] = line[find_nth_instance(line, " ", 2)+1:]
+                elif "--cpus-per-task" in line:
+                    job_info["cpus_per_task"] = line[find_nth_instance(line, " ", 2)+1:]
+            else:
+                break
+    return job_info
 
-def plot_profiling_data(job_path: str):
-    jobs_info = get_profiling_data(job_path)
-    for job_number, job_info in jobs_info.items():
-        for key in job_info.keys():
-            if "Task" in key and len(job_info[key]) > 2:
-                fig, ax = plt.subplots(nrows=3, ncols=1, layout='constrained', sharex=True, figsize=(10, 8))
-                fig.suptitle(f"Step {job_number}: {job_info['workpath']}")
-                try:
-                    cpu_count = int(job_info["cpus_per_task"])
-                except:
-                    cpu_count = 1
-                ax[0].fill_between(job_info[key]["ElapsedTime"],
-                         job_info[key]["CPUUtilization"]/cpu_count,
-                         0, linestyle='-', alpha=0.9)
-                ax[0].axhline(y=100, color = 'k', linestyle='--')
-                ax[0].set_title("Mean CPU usage")
-                ax[0].set_ylim(0,120)
-                ax[0].set_ylabel("% CPU Utilization", rotation=90)
 
-                ax[1].fill_between(job_info[key]["ElapsedTime"],
-                                   (job_info[key]["RSS"] + job_info[key]["Pages"]*4)/1000000,
-                         0, label="RAM usage", linestyle='-', alpha=0.9)
-                ax[1].set_title("RAM usage")
-                ax[1].axhline(y=(float(job_info["memory"])/1000), color = 'k', linestyle='--', label="Requested ram")
-                ax[1].set_ylabel("GB", rotation=90)
-                ax[1].set_ylim(0,(float(job_info["memory"])/1000) * 1.2)
-                ax[1].legend()
+def plot_profiling_data(job_path = None, job_id = None, sbatch_script = None):
+    if job_id is not None and sbatch_script is not None:
+        jobs_info = get_profiling_data_by_id(job_id, sbatch_script)
+        return jobs_info
+    elif job_id is not None:
+        jobs_info = get_profiling_data_by_path(job_path)
+        for job_number, job_info in jobs_info.items():
+            for key in job_info.keys():
+                if "Task" in key and len(job_info[key]) > 2:
+                    fig, ax = plt.subplots(nrows=3, ncols=1, layout='constrained', sharex=True, figsize=(10, 8))
+                    fig.suptitle(f"Step {job_number}: {job_info['workpath']}")
+                    try:
+                        cpu_count = int(job_info["cpus_per_task"])
+                    except:
+                        cpu_count = 1
+                    ax[0].fill_between(job_info[key]["ElapsedTime"],
+                             job_info[key]["CPUUtilization"]/cpu_count,
+                             0, linestyle='-', alpha=0.9)
+                    ax[0].axhline(y=100, color = 'k', linestyle='--')
+                    ax[0].set_title("Mean CPU usage")
+                    ax[0].set_ylim(0,120)
+                    ax[0].set_ylabel("% CPU Utilization", rotation=90)
 
-                ax[2].fill_between(job_info[key]["ElapsedTime"],
-                         job_info[key]["ReadMB"]/1000,
-                         0, label="Read", linestyle='-', alpha=0.9, color='b')
-                ax[2].fill_between(job_info[key]["ElapsedTime"], 0,
-                         -job_info[key]["WriteMB"]/1000, label="Write", linestyle='-', alpha=0.9, color='r')
-                ax[1].axhline(y=0, color = 'k', linestyle='--')
-                ax[2].set_title("I/O usage")
-                ax[2].set_ylabel("GB", rotation=90)
-                ax[2].set_xlabel("Elapsed time (s)")
-                ax[2].legend()
+                    ax[1].fill_between(job_info[key]["ElapsedTime"],
+                                       (job_info[key]["RSS"] + job_info[key]["Pages"]*4)/1000000,
+                             0, label="RAM usage", linestyle='-', alpha=0.9)
+                    ax[1].set_title("RAM usage")
+                    ax[1].axhline(y=(float(job_info["memory"])/1000), color = 'k', linestyle='--', label="Requested ram")
+                    ax[1].set_ylabel("GB", rotation=90)
+                    ax[1].set_ylim(0,(float(job_info["memory"])/1000) * 1.2)
+                    ax[1].legend()
 
-                plt.savefig(f"{job_path}/slurm_profiling/{job_number}_{key}.png")
+                    ax[2].fill_between(job_info[key]["ElapsedTime"],
+                             job_info[key]["ReadMB"]/1000,
+                             0, label="Read", linestyle='-', alpha=0.9, color='b')
+                    ax[2].fill_between(job_info[key]["ElapsedTime"], 0,
+                             -job_info[key]["WriteMB"]/1000, label="Write", linestyle='-', alpha=0.9, color='r')
+                    ax[1].axhline(y=0, color = 'k', linestyle='--')
+                    ax[2].set_title("I/O usage")
+                    ax[2].set_ylabel("GB", rotation=90)
+                    ax[2].set_xlabel("Elapsed time (s)")
+                    ax[2].legend()
+
+                    plt.savefig(f"{job_path}/slurm_profiling/{job_number}_{key}.png")
+    else:
+        raise Exception("No job_path OR job_id with sbatch_script given.")
     return jobs_info
